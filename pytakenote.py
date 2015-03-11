3d44f0588395e4f317abff74233dc881f4e9e792
Nasef Khan <nasef@Asus-Debian>
2015-03-11 00:35:04 +0000

    Changed error messages

--- pytakenote.py
+++ pytakenote.py
@@ -228,7 +228,7 @@ def main():
     elif sys.argv[1].upper() in ("DEL", "DELETE"):
         if len(sys.argv) != 3:
             print("Error... To delete please specify note ID")
-            print("e.g. - 'delete 2'")
+            print("e.g. - './pytakenote delete 2'")
             sys.exit(2)
         try:
             id_to_delete = int(sys.argv[2])
@@ -239,7 +239,7 @@ def main():
     elif sys.argv[1].upper() == "EDIT":
         if len(sys.argv) != 3:
             print("Error... To edit a note please specify note ID")
-            print("e.g. - 'edit 2'")
+            print("e.g. - './pytakenote edit 2'")
             sys.exit(2)
         try:
             id_to_edit = int(sys.argv[2])
@@ -249,13 +249,13 @@ def main():
             edit_note(id_to_edit)
     elif sys.argv[1].upper() == "SHOW":
         if len(sys.argv) != 3:
-            print("Error... To edit a note please specify note ID")
-            print("e.g. - 'edit 2'")
+            print("Error... To show a note please specify note ID")
+            print("e.g. - './pytakenote show 2'")
             sys.exit(2)
         try:
             id_to_show = int(sys.argv[2])
         except:
-            print("Error... The ID is an integer")
+            print("Error... Please enter an integer")
         else:
             show_note(id_to_show)
 
