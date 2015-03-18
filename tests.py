27fb943fe3c1a4a1685a28463304d98ce4ab713f
Axom Khan <axom@Debian-7-Desktop>
2015-03-18 23:58:26 +0000

    Added test for get_current_datetime function

--- tests.py
+++ tests.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 import unittest
-from pytakenote import *
+import pytakenote 
 
 class test_functions(unittest.TestCase):
     def setUp(self):
         print("Testing toplevel pytakenote functions")
 
     def test_get_current_date_time(self):
-        curr_time = get_current_datetime()
-        self.assertEqual(str, type(curr_time))
-        self.assertIn(":", curr_time)
+        curr_time = pytakenote.get_current_datetime()
+        # 2015-03-18 23:48:27
+        self.assertRegex(curr_time, r"\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}", msg="Date should be in the following format: 'yyyy-mm-dd hh:mm:ss'")
     
     def tearDown(self):
         print("Testing finished")
