2bc81f53c5cf569e44f8f174db883cd844b95869
Nasef Khan <nasef@debian8-vm>
2015-03-26 22:34:51 +0000

    - Argument handling now done by argparse - Streamlined new db creation and took out unnecessary functions

--- pytakenote.py
+++ pytakenote.py
@@ -8,31 +8,29 @@
 
 __author__ = "Nasef Khan (inbox@nakhan.net)"
 __license__ = 'GPL3' # see http://www.gnu.org/licenses/gpl.html
-__version__ = "0.2.0" 
+__version__ = "0.3.0" 
 
 import sys
-#import pdb
 import sqlite3
 import tempfile
 import os
+import argparse
 from datetime import datetime
 from subprocess import call
+import ipdb
 
-#pdb.set_trace()
-
-#db_filepath = ""
 db_name = ".pytakenote.db"
-file_location = os.path.dirname(os.path.abspath(__file__))
 
 class Note:
-    def __init__(self, id_=None):
+    def __init__(self, db, id_=None):
+        self.db = db
         if id_: # load note from db if note id id_ exists
             self.load(id_)
         else:
             self.create_new()
 
     def create_new(self):
-        self.id_ = get_unused_key() # to re-use deleted ids
+        self.id_ = get_unused_key(self.db) # to re-use deleted ids
         self.title = input("Please enter a title: ")
         self.body = get_body()
         self.date_time = get_current_datetime()
@@ -40,8 +38,10 @@ class Note:
 
     def load(self, id_):
         '''load note from db'''
-        conn, c = dbconn()
+        conn, c = dbconn(self.db)
         sql = "SELECT id, title, body FROM Notes WHERE id=?"
+        #ipdb.set_trace()
+        id_ = str(id_)
         data = c.execute(sql, (id_)).fetchone()
         conn.close()
         if data:
@@ -60,7 +60,7 @@ class Note:
         self.save_to_db()
     
     def delete(self):
-        conn, c = dbconn()
+        conn, c = dbconn(self.db)
         sql = "DELETE FROM Notes WHERE id=?"
         c.execute(sql, (self.id_,))
         conn.commit()
@@ -69,7 +69,7 @@ class Note:
     def check_note(self, id_):
         ''' Check if any data exists at id id_ in table'''
         #pdb.set_trace()
-        conn, c = dbconn()
+        conn, c = dbconn(self.db)
         sql = "SELECT id FROM Notes WHERE id=?"
         result = c.execute(sql, (id_,)).fetchone()
         conn.close()
@@ -83,7 +83,7 @@ class Note:
         print("Body: %s" % self.body)
 
     def save_to_db(self):
-        conn, c = dbconn()
+        conn, c = dbconn(self.db)
         if self.id_:
             if self.check_note(self.id_): # note being edited
                 sql = "UPDATE Notes SET title=?, body=?, datetime=? WHERE id=?"
@@ -97,8 +97,8 @@ class Note:
         conn.commit()
         conn.close()
 
-    def print_all_notes():
-        conn, c = dbconn()
+    def print_all_notes(db):
+        conn, c = dbconn(db)
         results = c.execute("SELECT id, title, body, datetime FROM Notes;")
         notes = results.fetchall()
         conn.close()
@@ -132,38 +132,13 @@ def search_db_file():
         return db_filepath
 
 
-def dbconn(sqlite_file=None):
-    if not sqlite_file:
-        sqlite_file=search_db_file()
+def dbconn(sqlite_file):
     conn = sqlite3.connect(sqlite_file)
     c = conn.cursor()
     return (conn, c)
 
 
-def user_input():
-    return input(">> ")
-
-
-def ask_db_location(user_input):
-    print("\nWhere do you wish to save the database file?")
-    print("1 - Same location as Python script (%s)" % file_location)
-    print("2 - Your home Directory (%s)" % os.getenv("HOME"))
-    print("")
-    chosen = 0
-    while not chosen:
-        choice = user_input()
-        choice = choice.strip()
-        if choice in ("1", "2"):
-            chosen = 1
-        else:
-            print("Enter 1 or 2\n")
-    choice = int(choice)
-    return ["./"+db_name, os.getenv("HOME") + "/" +  db_name][choice-1]
-
-
-def create_db(filepath=None):
-    if not filepath:
-        filepath = ask_db_location(user_input)
+def create_db(filepath):
     create_table_sql = "CREATE TABLE Notes (id INTEGER PRIMARY KEY, title TEXT, body TEXT, datetime DATETIME);"
     conn, c = dbconn(filepath)
     try:
@@ -200,9 +175,9 @@ def get_body(content=None):
     return body
 
 
-def get_unused_key():
+def get_unused_key(db):
     '''Reuse deleted keys'''
-    conn, c = dbconn()
+    conn, c = dbconn(db)
     sql = "SELECT id FROM Notes ORDER BY id ASC ";
     results = c.execute(sql)
     keys = [int(i[0]) for i in results.fetchall()]
@@ -214,48 +189,53 @@ def get_unused_key():
         return del_keys[0]
 
 
-def main():
+def main(argv):
+    parser = argparse.ArgumentParser(description=__doc__)
+    group = parser.add_mutually_exclusive_group()
 
-    if not search_db_file():
+    parser.add_argument("-c", "--db", "--config", type=str,  
+            default=os.getenv("HOME") + "/" + db_name,
+            help="Specify a location of a config/db file to use")
+    group.add_argument("-l", "--list", action="store_true", 
+            help="Print out all notes")
+    group.add_argument("-a", "--add", action="store_true", 
+            help="Add a note")
+    group.add_argument("-d", "--delete", "--remove", type=int, 
+            help="Delete a note (specify an ID)")
+    group.add_argument("-e", "--edit", type=int, 
+            help="Edit a note (specify an ID)")
+    group.add_argument("-s", "--show", type=int, 
+            help="Show a note (specify an ID)")
+
+    args = parser.parse_args(argv)
+
+    #ipdb.set_trace()
+    db = args.db
+
+    if not os.path.isfile(args.db):
         print("Creating notetaker Database...")
-        if create_db():
-            print("Database created succesfully.\n")
-            main()
-            sys.exit(0)
-        else:
-            print("Database could not be created...")
-            sys.exit(1)
-
-    if len(sys.argv) == 1:
-        Note.print_all_notes()
+        create_db(db)
+        print("Database created succesfully.\n")
+        main(argv)
         sys.exit(0)
-        
-
-    if sys.argv[1].upper() == "ADD":
-        new_note = Note()
-    elif sys.argv[1].upper() in ("DEL", "DELETE"):
-        if len(sys.argv) != 3:
-            print("Error... To delete a note please specify note ID. For example:")
-            print("./pytakenote.py delete 2")
-            sys.exit(2)
-        note = Note(sys.argv[2])
+    
+    if args.list:
+        Note.print_all_notes(db)
+    elif args.add:
+        Note(db)
+    elif args.delete:
+        note = Note(db, args.delete)
         note.delete()
-    elif sys.argv[1].upper() == "EDIT":
-        if len(sys.argv) != 3:
-            print("Error... To edit a note please specify note ID. For example:")
-            print("./pytakenote.py edit 2")
-            sys.exit(2)
-        note  = Note(sys.argv[2])
+    elif args.edit:
+        note = Note(db, args.edit)
         note.edit()
-    elif sys.argv[1].upper() == "SHOW":
-        if len(sys.argv) != 3:
-            print("Error... To show a note please specify note ID. For example:")
-            print("./pytakenote.py show 2")
-            sys.exit(2)
-        note = Note(sys.argv[2])
+    elif args.show:
+        note = Note(db, args.show)
         note.print_()
     else:
-        print("Whaaaat?")
+        Note.print_all_notes(db)
+
+
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
