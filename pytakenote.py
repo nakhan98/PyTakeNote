d9b3fc385cc7c3aef2c5c672218fef7d0b5136d8
Nasef Khan <nasef@debian-8-dev>
2016-06-29 13:10:50 +0100

    Fixed pep8 issues

--- pytakenote.py
+++ pytakenote.py
@@ -6,11 +6,6 @@
 - Needed an upgrade from my notes.txt file
 """
 
-__title__ = "PyTakeNote"
-__version__ = "0.3.1" 
-__author__ = "Nasef Khan (inbox@nakhan.net)"
-__license__ = 'GPL3' # see http://www.gnu.org/licenses/gpl.html
-
 import sys
 import sqlite3
 import tempfile
@@ -19,18 +14,25 @@ import argparse
 from datetime import datetime
 from subprocess import call
 
+__title__ = "PyTakeNote"
+__version__ = "0.3.1"
+__author__ = "Nasef Khan (inbox@nakhan.net)"
+__license__ = 'GPL3'  # see http://www.gnu.org/licenses/gpl.html
+
+
 db_name = ".pytakenote.db"
+
 
 class Note:
     def __init__(self, db, id_=None):
         self.db = db
-        if id_: # load note from db if note id id_ exists
+        if id_:  # load note from db if note id id_ exists
             self.load(id_)
         else:
             self.create_new()
 
     def create_new(self):
-        self.id_ = get_unused_key(self.db) # to re-use deleted ids
+        self.id_ = get_unused_key(self.db)  # to re-use deleted ids
         self.title = input("Please enter a title: ")
         self.body = get_body()
         self.date_time = get_current_datetime()
@@ -40,7 +42,6 @@ class Note:
         '''load note from db'''
         conn, c = dbconn(self.db)
         sql = "SELECT id, title, body FROM Notes WHERE id=?"
-        #ipdb.set_trace()
         id_ = str(id_)
         data = c.execute(sql, (id_)).fetchone()
         conn.close()
@@ -58,7 +59,7 @@ class Note:
         self.body = get_body(self.body)
         self.date_time = get_current_datetime()
         self.save_to_db()
-    
+
     def delete(self):
         conn, c = dbconn(self.db)
         sql = "DELETE FROM Notes WHERE id=?"
@@ -68,7 +69,6 @@ class Note:
 
     def check_note(self, id_):
         ''' Check if any data exists at id id_ in table'''
-        #pdb.set_trace()
         conn, c = dbconn(self.db)
         sql = "SELECT id FROM Notes WHERE id=?"
         result = c.execute(sql, (id_,)).fetchone()
@@ -85,12 +85,15 @@ class Note:
     def save_to_db(self):
         conn, c = dbconn(self.db)
         if self.id_:
-            if self.check_note(self.id_): # note being edited
+            if self.check_note(self.id_):  # note being edited
                 sql = "UPDATE Notes SET title=?, body=?, datetime=? WHERE id=?"
-                c.execute(sql, (self.title, self.body, self.date_time, self.id_))
-            else: # new note being saved as lowest deleted id
-                sql = "INSERT INTO Notes(id, title, body, datetime) values(?, ?, ?, ?)"
-                c.execute(sql, (self.id_, self.title, self.body, self.date_time))
+                c.execute(sql, (self.title, self.body, self.date_time,
+                                self.id_))
+            else:  # new note being saved as lowest deleted id
+                sql = ("INSERT INTO Notes(id, title, body, datetime)"
+                       " values(?, ?, ?, ?)")
+                c.execute(sql, (self.id_, self.title, self.body,
+                                self.date_time))
         else:
             sql = "INSERT INTO Notes(title, body, datetime) values(?, ?, ?)"
             c.execute(sql, (self.title, self.body, self.date_time))
@@ -108,26 +111,26 @@ class Note:
         print("%-3s %-48s %-16s" % ("ID", "Title", "Date"))
         print("-" * 72)
         for note in notes:
-            print("%-3d %-48s %-16s" % (note[0], shorten_string(note[1]), note[3]))
-        #print("\n")
+            print("%-3d %-48s %-16s" % (note[0], shorten_string(note[1]),
+                                        note[3]))
 
 
 def get_current_datetime():
-   return str( datetime.now() )[:-7]
+    return str(datetime.now())[:-7]
 
 
 def shorten_string(string):
     ''' To stop titles overrunning'''
     if len(string) > 49:
         string = string[:45] + "..."
-    return string 
+    return string
 
 
 def search_db_file():
     if os.path.isfile("./"+db_name):
         db_filepath = "./"+db_name
         return db_filepath
-    elif os.path.isfile( os.getenv("HOME") + "/" + db_name ):
+    elif os.path.isfile(os.getenv("HOME") + "/" + db_name):
         db_filepath = os.getenv("HOME") + "/" + db_name
         return db_filepath
 
@@ -139,7 +142,8 @@ def dbconn(sqlite_file):
 
 
 def create_db(filepath):
-    create_table_sql = "CREATE TABLE Notes (id INTEGER PRIMARY KEY, title TEXT, body TEXT, datetime DATETIME);"
+    create_table_sql = ("CREATE TABLE Notes (id INTEGER PRIMARY KEY, "
+                        "title TEXT, body TEXT, datetime DATETIME);")
     conn, c = dbconn(filepath)
     try:
         ret_cur = c.execute(create_table_sql)
@@ -149,7 +153,7 @@ def create_db(filepath):
         conn.close()
         return False
     else:
-        _ = ret_cur.fetchall()
+        ret_cur.fetchall()
         conn.close()
         return True
 
@@ -178,7 +182,7 @@ def get_body(content=None):
 def get_unused_key(db):
     '''Reuse deleted keys'''
     conn, c = dbconn(db)
-    sql = "SELECT id FROM Notes ORDER BY id ASC ";
+    sql = "SELECT id FROM Notes ORDER BY id ASC;"
     results = c.execute(sql)
     keys = [int(i[0]) for i in results.fetchall()]
     conn.close()
@@ -193,24 +197,23 @@ def main(argv):
     parser = argparse.ArgumentParser(prog=__title__, description=__doc__)
     group = parser.add_mutually_exclusive_group()
 
-    parser.add_argument("-c", "--db", "--config", type=str,  
-            default=os.getenv("HOME") + "/" + db_name,
-            help="Specify a location of a config/db file to use")
-    group.add_argument("-l", "--list", action="store_true", 
-            help="Print out all notes")
-    group.add_argument("-a", "--add", action="store_true", 
-            help="Add a note")
-    group.add_argument("-d", "--delete", "--remove", type=int, 
-            help="Delete a note (specify an ID)")
-    group.add_argument("-e", "--edit", type=int, 
-            help="Edit a note (specify an ID)")
-    group.add_argument("-s", "--show", type=int, 
-            help="Show a note (specify an ID)")
-    parser.add_argument("-V", '--version', action='version', version='%(prog)s ' + __version__)
+    parser.add_argument("-c", "--db", "--config", type=str,
+                        default=os.getenv("HOME") + "/" + db_name,
+                        help="Specify a location of a config/db file to use")
+    group.add_argument("-l", "--list", action="store_true",
+                       help="Print out all notes")
+    group.add_argument("-a", "--add", action="store_true", help="Add a note")
+    group.add_argument("-d", "--delete", "--remove", type=int,
+                       help="Delete a note (specify an ID)")
+    group.add_argument("-e", "--edit", type=int,
+                       help="Edit a note (specify an ID)")
+    group.add_argument("-s", "--show", type=int,
+                       help="Show a note (specify an ID)")
+    parser.add_argument("-V", '--version', action='version',
+                        version='%(prog)s ' + __version__)
 
     args = parser.parse_args(argv)
 
-    #ipdb.set_trace()
     db = args.db
 
     if not os.path.isfile(args.db):
@@ -219,7 +222,7 @@ def main(argv):
         print("Database created succesfully.\n")
         main(argv)
         sys.exit(0)
-    
+
     if args.list:
         Note.print_all_notes(db)
     elif args.add:
